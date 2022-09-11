7e75b80a7ebef6784c0f26f0d236684ee025e49c
Samglad2013 <gladysowhotemu@gmail.com>
2022-09-11 13:31:33 -0700

    innitial commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,13 +12,9 @@ char C = 'A';
 while (c <= 'z')
 {
 putchar(c);
-c++;	
-}
+c++;										}
 while (C <= 'Z')
-{
-putchar(C);
-C++;
-}
+{										putchar(C);									C++;										}
 putchar('\n');
 return (0);
 }
