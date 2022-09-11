29d7694ddb1b6dc28ae8d8a5d69cb850d31025d0
Samglad2013 <gladysowhotemu@gmail.com>
2022-09-11 13:05:13 -0700

    innitial commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,8 @@
 
 /**
  * main - Entry point
- *   
- * Return: Always 0(Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -16,7 +16,6 @@ if (n > 0)
 printf("%d is positive\n", n);
 else if (n == 0)
 printf("%d is zero\n", n);
-else
-printf("%d is negative\n", n);
+else										printf("%d is negative\n", n);
 return (0);
 }
