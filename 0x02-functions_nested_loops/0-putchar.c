321d11a2c29693a5281c2207cfd1ff41c534af3e
Samglad2013 <gladysowhotemu@gmail.com>
2022-09-13 05:27:35 -0700

    innitial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,14 @@
-#include "main.h"
+#include "main.c"
 
 int main(void)
 {
-		char s[] = "_putchar";
-			int i = 0;
-
-				while (s[i] != '\0')
-						{
-									_putchar(s[i]);
-											i++;
-												}
-					_putchar(10);
-
-						return (0);
+char s[] = "_putchar";
+int i = 0;
+while (s[i] != '\0')
+{
+_putchar(s[i]);
+i++;
+}
+_putchar(10);
+return (0);
 }
