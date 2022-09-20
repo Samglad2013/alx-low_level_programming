1ae0d3ef2e2cc7a19c56c12d1b35ab9af57e75c6
Samglad2013 <gladysowhotemu@gmail.com>
2022-09-20 06:01:42 -0700

    innitial commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,16 +2,16 @@
 
 /**
  * _puts - print a given string to screen
- *
- * @str: passed pointer argument for string
- *  Return: void
-*/
+ 
+*@str: passed pointer argument for string
+ *Return: void
+ *      */
 void _puts(char *str)
 {
 int len = 0;
 while (*(str + len) != '\0')
+{
 _putchar(*(str + len));
-len++;
-}
+len++;										}
 _putchar('\n');
 }
