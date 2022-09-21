d61606be7092a1112e36fcbadaf560e4c71972ab
Samglad2013 <gladysowhotemu@gmail.com>
2022-09-21 01:36:10 -0700

    innitial commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,19 +1,39 @@
 #include "main.h"
- **
- * _strcpy - copies string to given memory location
- * @dest: where the string needs to be copied
- * @src: where the string is
- *
- * Return: char
+
+/**
+ * _atoi - converts the first set of digits in a string to an integer,
+ * taking into account the sign of the number
+ * @s: string to be converted
+ * Return: int containing the converted number
  */
- char *_strcpy(char *dest, char *src)
+int _atoi(char *s)
 {
-int len = 0;
-while (*(src + len) != '\0')
+int index, ind2;
+unsigned int res;
+int sign = 1;
+char now;
+index = 0;
+res = 0;
+while (*(s + index) != '\0')
 {
-*(dest + len) = *(src + len);
-len++;
+now = *(s + index);
+if (now == '-')
+{
+sign *= -1;
 }
-*(dest + len) = *(src + len);
-return (dest);
+if (now >= '0' && now <= '9')
+{
+ind2 = index;
+while (*(s + ind2) > 47 && *(s + ind2) < 58)
+{
+res = (res * 10) + *(s + ind2) - '0';
+ind2++;
+}
+break;
+}
+index++;
+}
+if (sign < 0)
+res *= sign;
+return (res);
 }
